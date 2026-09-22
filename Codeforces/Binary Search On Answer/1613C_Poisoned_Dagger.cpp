a94a360794ce381ae863c01f5a45b3fe08b11168
Yihoyzz33 <143344448+Hideonbush3333@users.noreply.github.com>
2026-09-22 23:21:06 +0800

    bsoa 0

--- Codeforces/Binary Search On Answer/1613C_Poisoned_Dagger.cpp
+++ Codeforces/Binary Search On Answer/1613C_Poisoned_Dagger.cpp
@@ -1,10 +1,17 @@
-/*CF 1613C C_Poisoned_Dagger 1200*/
+/*CF 1613C C_Poisoned_Dagger 1200*
+二分答案FFFFFTTTTT 
+毒素持续时间越长造成伤害越高,越短造成伤害越低，套板子1
+这里重点讲下毒素持续时间的计算，一开始我想的是遍历所有时间，但是肯定超时
+如果只遍历a[i],那么我们只需要看这次毒素实际贡献了几点伤害
+如果是在上次毒素结束时间之后就是加上全部的mid,否则加上补上的实际伤害时间
+最后看是否造成>=h的伤害就行了
+*/
 #include<bits/stdc++.h>
 using namespace std;
-int t,n,h;
+long long t,n,h;
 vector <int> a;
-bool check(int mid,int n){
-    int ans = mid,last = a[1] + mid - 1;
+bool check(long long mid,long long n){
+    long long ans = mid,last = a[1] + mid - 1;
     for(int i = 2;i <= n;i++){
         if(a[i] > last){
             ans += mid;
@@ -26,9 +33,9 @@ int main(){
         for(int i = 1;i <= n;i++){
             cin >> a[i];
         }
-        int left = 1,right = h;
+        long long left = 1,right = h;
         while(left < right){
-            int mid = left + (right - left) / 2;
+            long long mid = left + (right - left) / 2;
             if(check(mid,n)){
                 right = mid;
             }
